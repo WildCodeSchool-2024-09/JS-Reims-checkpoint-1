ca4f69409f5581a461284cf320d95ecbff71b8fd
jottiana <11.paradise.avenue@gmail.com>
2024-10-07 12:01:43 +0200

    finish JS step 2 in part 1

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -5,3 +5,19 @@ function changeAvatar() {
 }
 
 avatar.addEventListener("click", changeAvatar);
+
+
+function replaceName() {
+  const firstName = prompt("Enter your name :");
+  const name = document.getElementById("firstname");
+  name.setAttribute("style", "color: white");
+  name.textContent = firstName;
+}
+
+const description = document.getElementsByClassName("description")[0];
+
+const button = document.createElement("button");
+button.textContent = "Modify text and color";
+button.addEventListener("click", replaceName);
+
+description.appendChild(button);
