fecef473a3a149986ddc885a8420c59d524aae32
jottiana <11.paradise.avenue@gmail.com>
2024-10-07 14:48:08 +0200

    there was just a missing sign =

--- algo/2/isLeapYear.js
+++ algo/2/isLeapYear.js
@@ -8,7 +8,7 @@ Rappel : Pour vérifier si un nombre est divisible par un autre, tu peux utilise
 */
 
 function isLeapYear(year) {
-  return (year % 4 === 0 && year % 100 != 0) || year % 400 === 0
+  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0
 }
 
 module.exports = isLeapYear;
