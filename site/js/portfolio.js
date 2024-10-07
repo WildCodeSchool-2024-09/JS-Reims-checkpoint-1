a06553c2ad7712ca98b0f51f0b813eb850caa533
paulcnl <paul.canarelli.2504@gmail.com>
2024-10-07 12:12:31 +0200

    site-js

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -1,7 +1,37 @@
-const image = document.getElementById("image/avatar-bis.png");
-const myButton = document.getElementById("avbu");
+document.addEventListener("DOMContentLoaded", function () {
+  const avatar = document.getElementById("avatar");
 
-myButton.addEventListener("click", function () {
-  image.src =
-    "https://cms.imgworlds.com/assets/a5366382-0c26-4726-9873-45d69d24f819.jpg?key=home-gallery";
+  const images = ["image/avatar-bis.png", "image/avatar.svg"];
+
+  let currentIndex = 0;
+
+  function toggleAvatar() {
+    currentIndex = currentIndex === 0 ? 1 : 0;
+
+    avatar.src = images[currentIndex];
+  }
+
+  avatar.addEventListener("click", toggleAvatar);
 });
+
+function changeName() {
+  const newName = prompt("Entrez votre nom :");
+
+  if (newName) {
+    const nameElement = document.getElementById("firstname");
+    nameElement.textContent = newName;
+
+    nameElement.style.color = getRandomColor();
+  }
+}
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+window.onload = changeName;
