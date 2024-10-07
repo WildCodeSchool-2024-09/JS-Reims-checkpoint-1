62776b343b1b86981aa6bdb37d631154ace98b2a
paulcnl <paul.canarelli.2504@gmail.com>
2024-10-07 10:49:33 +0200

    site-css

---
+++ site/js/portfolio.js
@@ -0,0 +1,7 @@
+const image = document.getElementById("image/avatar-bis.png");
+const myButton = document.getElementById("avbu");
+
+myButton.addEventListener("click", function () {
+  image.src =
+    "https://cms.imgworlds.com/assets/a5366382-0c26-4726-9873-45d69d24f819.jpg?key=home-gallery";
+});
